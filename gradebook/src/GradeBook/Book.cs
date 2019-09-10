19ad33282cf3a99707de1d1e5a7bc1b1d76c3d4c
João Paulo Oliveira <jpaulo.oliveira@brainfarma.ind.br>
2019-09-10 09:41:16 -0300

    Playing Jumping with break, continue and goto

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -30,14 +30,27 @@ namespace GradeBook
             result.High = double.MinValue;
             result.Low = double.MaxValue;
             //foreach (var grade in grades)
-            var index = 0;
             for(var index = 0; index < grades.Count; index +=1)
             {
+                if(grades[index] == 42.1)
+                {
+                    continue;
+                }
+                if(grades[index] == 55.5)
+                {
+                    break;
+                }
+                if(grades[index] == 66.6)
+                {
+                    goto done;
+                }
+
                 result.Low = Math.Min(grades[index], result.Low);
                 result.High = Math.Max(grades[index], result.High);
                 result.Average += grades[index];
             }
             result.Average /= grades.Count;
+            done:
 
             return result;
         
